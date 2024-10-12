8989c00c20f35026af27f48ff8de06c0d8365c28
FaTiMa! <81984329+Fatima-zn@users.noreply.github.com>
2024-10-12 19:52:41 +0100

    file added

---
+++ AuthScreenUI/Colors/pallete.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Pallete{
+  static const textFieldColor = Color(0xff626262);
+  static const fieldBackColor = Color(0xfff1f4ff);
+  static const buttonBlue = Color(0xff1f41bb);
+  static const iconsBackColor = Color(0xffececec);
+  static const darkTextColor = Color(0xff000000);
+  static const lightTextColor = Color(0xffffffff);
+}
